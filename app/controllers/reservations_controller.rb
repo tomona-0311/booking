ce5361672480274174b284d76209f5af355d05af
tomona-0311 <karafuru0311@gmail.com>
2022-12-07 11:04:48 +0900

    予約

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,5 +9,22 @@ class ReservationsController < ApplicationController
      @reservation = Reservation.new
 
     end
+    def create
+      @reservation = Reservation.new(params.require(:reservation).permit(:room_id,:user_id,:start_date,:end_date,:price,:total_price,:people,:total_day))
+      if params[:back] || !@reservation.save
+        render "index"
+      else
+        redirect_to  reservation_path(@reservation), notice: "予約が完了しました"
+      end
+    end
+
+
+
+    private
+
+    def reservation_params
+      params.require(:reservation).permit(:room_id,:user_id,:start_date,:end_date,:people)
+    end
+
 
 end
