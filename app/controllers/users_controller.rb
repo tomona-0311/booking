a5c3340b85a2d153d9a20b6d01d9c70744e6d05b
tomona-0311 <karafuru0311@gmail.com>
2022-12-28 20:24:36 +0900

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
-
-
   before_action :authenticate_user!
   def account
     @user = current_user
   end
   def profile
     @user = current_user
-
   end
+
   def login
     @q = Room.ransack(params[:q])
     @rooms = @q.result(distinct: true)
